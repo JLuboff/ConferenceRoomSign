d7ec6666892269a7f544d63c6e40e277eb544754
Jason L <jluboff@gmail.com>
2018-02-02 16:07:29 -0800

    Created views and reconfigured to work with mongo

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ const express = require('express'),
       routes = require('./routes/routes'),
       MongoClient = require('mongodb').MongoClient,
       bodyParser = require('body-parser'),
+      hbs = require('hbs'),
       port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static("public"));
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(`mongodb://localhost:27017`, (err, client)=>{
-  if(err) throw err;
-  var db = client.db('events');
+MongoClient.connect(`mongodb://localhost:27017`, (err, client) => {
+	if (err) throw err;
+	var db = client.db('events');
 
-  routes(app, db);
-})
+	routes(app, db);
+});
 
-app.listen(port, ()=>{
-  console.log(`Listening on port ${port}`);
-})
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,49 @@
-
+const moment = require('moment');
 module.exports = (app, db) => {
-  app.route('/createEvents').post((req, res) => {
-    console.log("Post route reached");
-    console.log(req.body);
-    let insert = [];
+	app.route('/meetingEntry/:room').get((req, res) => {
+		res.render('meetingEntry.hbs', {
+			Title: `Conference Room ${req.params.room}`,
+			url: req.params.room.toLowerCase()
+		});
+	});
+  
+	app.route('/createEvents/:room').post((req, res) => {
+		let insert = [];
 
-    for(let i = 0; i < req.body.meetingTitle.length; i++){
-      let obj = {};
+		for (let i = 0; i < req.body.meetingTitle.length; i++) {
+			let obj = {};
+			let startTime = `${req.body.startTime[i]} ${req.body.startTimePeriod[i]}`;
 
-      obj["Title"] = req.body.meetingTitle[i];
-      obj['Start Time'] = `${req.body.startTime[i]} ${req.body.startTimePeriod[i]}`;
-      obj['End Time'] = `${req.body.endTime[i]} ${req.body.endTimePeriod[i]}`;
-      insert.push(obj);
-    }
-    console.log(insert);
+			obj['Title'] = req.body.meetingTitle[i];
+			obj['Start Time'] = startTime;
+			obj['End Time'] = `${req.body.endTime[i]} ${req.body.endTimePeriod[i]}`;
+			obj['Date'] = req.body.meetingDate[i];
+			obj['Room'] = req.params.room;
+			obj['SortField'] = moment(
+				`${req.body.meetingDate[i]} ${startTime}`,
+				'YYYY-MM-DD h:mm a'
+			).valueOf();
+			insert.push(obj);
+		}
 
-    db.collection('event').insertMany(insert);
-    db.collection('event').find({}).toArray((err, doc) => {
-      if(err) throw err;
-      console.log(doc);
-    })
-  })
-}
+		db.collection('event').insertMany(insert);
+	});
+
+	app.route('/roomDisplay/:room').get((req, res) => {
+		res.render('display.hbs', {
+			Title: `Conference Room ${req.params.room}`,
+			url: req.params.room.toLowerCase()
+		});
+	});
+
+	app.route('/getEvents/:room').get((req, res) => {
+		db
+			.collection('event')
+			.find({ Date: moment().format('YYYY-MM-DD'), Room: req.params.room })
+			.sort({ SortField: 1 })
+			.toArray((err, docs) => {
+				if (err) throw err;
+				res.json(docs);
+			});
+	});
+};
