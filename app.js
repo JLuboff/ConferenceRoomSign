006419be17e29beefa3e3f5066e7bcde7a6f9142
Jason L <jluboff@gmail.com>
2018-06-12 15:28:44 -0700

    Updated to account for training room and linted files

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express'),
-      routes = require('./routes/routes'),
-      MongoClient = require('mongodb').MongoClient,
-      bodyParser = require('body-parser'),
-      hbs = require('hbs'),
-      port = process.env.PORT || 3000;
+const express = require('express');
+const routes = require('./routes/routes');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+/* eslint-disable */
+const hbs = require('hbs');
+/* eslint-enable */
+const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public/images'));
+app.use(express.static(`${__dirname}/public/images`));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(`mongodb://localhost:27017`, (err, client) => {
-	if (err) throw err;
-	var db = client.db('events');
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) throw err;
+  const db = client.db('events');
 
-	routes(app, db);
+  routes(app, db);
 });
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,143 +1,150 @@
-const moment = require('moment'),
-			ObjectId = require('mongodb').ObjectId;
+const moment = require('moment');
+const { ObjectId } = require('mongodb');
 
 module.exports = (app, db) => {
-	app.route('/confSelect').get((req, res) => {
-		res.render('confSelect.hbs');
-	});
+  app.route('/confSelect').get((req, res) => {
+    res.render('confSelect.hbs');
+  });
 
-	app.route('/createEvents/:room').post((req, res) => {
-		let insert = [];
+  app.route('/createEvents/:room').post((req, res) => {
+    const insert = [];
 
-		for (let i = 0; i < req.body.meetingTitle.length; i++) {
-			let obj = {};
-			let startTime = `${req.body.startTime[i]} ${req.body.startTimePeriod[i]}`;
+    for (let i = 0; i < req.body.meetingTitle.length; i += 1) {
+      const obj = {};
+      const startTime = `${req.body.startTime[i]} ${req.body.startTimePeriod[i]}`;
 
-			obj['Title'] = req.body.meetingTitle[i];
-			obj['Start Time'] = startTime;
-			obj['End Time'] = `${req.body.endTime[i]} ${req.body.endTimePeriod[i]}`;
-			obj['Date'] = req.body.meetingDate[i];
-			obj['Room'] = req.params.room;
-			obj['SortField'] = moment(
-				`${req.body.meetingDate[i]} ${startTime}`,
-				'YYYY-MM-DD h:mm a'
-			).valueOf();
-			insert.push(obj);
-		}
+      obj.Title = req.body.meetingTitle[i];
+      obj['Start Time'] = startTime;
+      obj['End Time'] = `${req.body.endTime[i]} ${req.body.endTimePeriod[i]}`;
+      obj.Date = req.body.meetingDate[i];
+      obj.Room = req.params.room;
+      obj.SortField = moment(
+        `${req.body.meetingDate[i]} ${startTime}`,
+        'YYYY-MM-DD h:mm a',
+      ).valueOf();
+      insert.push(obj);
+    }
 
-		db.collection('event').insertMany(insert);
-		res.sendStatus(200);
-	});
+    db.collection('event').insertMany(insert);
+    res.sendStatus(200);
+  });
 
-	app.route('/deleteEvent/:room/:id').delete((req, res) => {
-		db
-			.collection('event')
-			.findOneAndDelete({ _id: ObjectId(req.params.id) })
-			.then(() => {
-				db
-					.collection('event')
-					.find({
-						Date: { $gte: moment().format('YYYY-MM-DD') },
-						Room: req.params.room
-					})
-					.sort({ SortField: 1 })
-					.toArray((err, data) => {
-						if (err) throw err;
-						data.forEach(e => {
-							e.Date = moment(e.Date).format('ddd, MMM DD YYYY');
-							e.longTitle = e.Title.length > 23 ? e.Title : false;
-							e.Title =
-								e.Title.length > 23 ? e.Title.slice(0, 23) + '...' : e.Title;
-						});
-						res.json(data);
-					});
-			});
-	});
+  app.route('/deleteEvent/:room/:id').delete((req, res) => {
+    db
+      .collection('event')
+      .findOneAndDelete({ _id: ObjectId(req.params.id) })
+      .then(() => {
+        db
+          .collection('event')
+          .find({
+            Date: { $gte: moment().format('YYYY-MM-DD') },
+            Room: req.params.room,
+          })
+          .sort({ SortField: 1 })
+          .toArray((err, data) => {
+            if (err) throw err;
+            data.forEach((e) => {
+              e.Date = moment(e.Date).format('ddd, MMM DD YYYY');
+              e.longTitle = e.Title.length > 23 ? e.Title : false;
+              e.Title =
+               e.Title.length > 23 ? `${e.Title.slice(0, 23)}...` : e.Title;
+            });
+            res.json(data);
+          });
+      });
+  });
 
-	app.route('/editEvents/:room').get((req, res) => {
-		db
-			.collection('event')
-			.find({
-				Date: { $gte: moment().format('YYYY-MM-DD') },
-				Room: req.params.room
-			})
-			.sort({ SortField: 1 })
-			.toArray((err, data) => {
-				if (err) throw err;
-				data.forEach(e => {
-					e.Date = moment(e.Date).format('ddd, MMM DD YYYY');
-					e.longTitle = e.Title.length > 23 ? e.Title : false;
-					e.Title =
-						e.Title.length > 23 ? e.Title.slice(0, 23) + '...' : e.Title;
-				});
-				res.render('editMeetings', {
-					data,
-					Title: `Conference Room ${req.params.room}`,
-					url: req.params.room.toLowerCase()
-				});
-			});
-	});
+  app.route('/editEvents/:room').get((req, res) => {
+    const { room } = req.params;
+    const title = room.toLowerCase() === 'training room' ? 'Training Room' : `Conference Room ${room}`;
 
-	app.route('/getEvents/:room').get((req, res) => {
-		db
-			.collection('event')
-			.find({ Date: moment().format('YYYY-MM-DD'), Room: req.params.room })
-			.sort({ SortField: 1 })
-			.toArray((err, docs) => {
-				if (err) throw err;
-				res.json(docs);
-			});
-	});
+    db
+      .collection('event')
+      .find({
+        Date: { $gte: moment().format('YYYY-MM-DD') },
+        Room: room,
+      })
+      .sort({ SortField: 1 })
+      .toArray((err, data) => {
+        if (err) throw err;
+        data.forEach((e) => {
+          e.Date = moment(e.Date).format('ddd, MMM DD YYYY');
+          e.longTitle = e.Title.length > 23 ? e.Title : false;
+          e.Title =
+           e.Title.length > 23 ? `${e.Title.slice(0, 23)}...` : e.Title;
+        });
+        res.render('editMeetings', {
+          data,
+          Title: title,
+          url: req.params.room.toLowerCase(),
+        });
+      });
+  });
 
-	app.route('/meetingEntry/:room').get((req, res) => {
-		res.render('meetingEntry.hbs', {
-			title: `Conference Room ${req.params.room}`,
-			url: req.params.room.toLowerCase()
-		});
-	});
+  app.route('/getEvents/:room').get((req, res) => {
+    db
+      .collection('event')
+      .find({ Date: moment().format('YYYY-MM-DD'), Room: req.params.room })
+      .sort({ SortField: 1 })
+      .toArray((err, docs) => {
+        if (err) throw err;
+        res.json(docs);
+      });
+  });
 
-	app.route('/roomDisplay/:room').get((req, res) => {
-		res.render('display.hbs', {
-			title: `Conference Room ${req.params.room}`,
-			url: req.params.room.toLowerCase()
-		});
-	});
+  app.route('/meetingEntry/:room').get((req, res) => {
+    const { room } = req.params;
+    const title = room.toLowerCase() === 'training room' ? 'Training Room' : `Conference Room ${room}`;
+    res.render('meetingEntry.hbs', {
+      title,
+      url: room.toLowerCase(),
+    });
+  });
 
-	app.route('/updateEvent/:room/:id').put((req, res) => {
-		let obj = {};
-		let date = moment(req.body.meetingDate).format('YYYY-MM-DD');
-		let startTime = `${req.body.startTime} ${req.body.startTimePeriod}`;
+  app.route('/roomDisplay/:room').get((req, res) => {
+    const { room } = req.params;
+    const title = room.toLowerCase() === 'training room' ? 'Training Room' : `Conference Room ${room}`;
+    res.render('display.hbs', {
+      title,
+      url: req.params.room.toLowerCase(),
+    });
+  });
 
-		obj['Title'] = req.body.meetingTitle;
-		obj['Start Time'] = startTime;
-		obj['End Time'] = `${req.body.endTime} ${req.body.endTimePeriod}`;
-		obj['Date'] = date;
-		obj['Room'] = req.params.room;
-		obj['SortField'] = moment(
-			`${date} ${startTime}`,
-			'YYYY-MM-DD h:mm a'
-		).valueOf();
-		db
-			.collection('event')
-			.findOneAndUpdate({ _id: ObjectId(req.params.id) }, { $set: obj })
-			.then(() => {
-				db
-					.collection('event')
-					.find({
-						Date: { $gte: moment().format('YYYY-MM-DD') },
-						Room: req.params.room
-					})
-					.sort({ SortField: 1 })
-					.toArray((err, data) => {
-						if (err) throw err;
-						data.forEach(e => {
-							e.Date = moment(e.Date).format('ddd, MMM DD YYYY');
-							e.longTitle = e.Title.length > 23 ? e.Title : false;
-							e.Title =
-								e.Title.length > 23 ? e.Title.slice(0, 23) + '...' : e.Title;
-						});
-						res.json(data);
-					});
-			});
-	});
+  app.route('/updateEvent/:room/:id').put((req, res) => {
+    const obj = {};
+    const date = moment(req.body.meetingDate).format('YYYY-MM-DD');
+    const startTime = `${req.body.startTime} ${req.body.startTimePeriod}`;
+
+    obj.Title = req.body.meetingTitle;
+    obj['Start Time'] = startTime;
+    obj['End Time'] = `${req.body.endTime} ${req.body.endTimePeriod}`;
+    obj.Date = date;
+    obj.Room = req.params.room;
+    obj.SortField = moment(
+      `${date} ${startTime}`,
+      'YYYY-MM-DD h:mm a',
+    ).valueOf();
+    db
+      .collection('event')
+      .findOneAndUpdate({ _id: ObjectId(req.params.id) }, { $set: obj })
+      .then(() => {
+        db
+          .collection('event')
+          .find({
+            Date: { $gte: moment().format('YYYY-MM-DD') },
+            Room: req.params.room,
+          })
+          .sort({ SortField: 1 })
+          .toArray((err, data) => {
+            if (err) throw err;
+            data.forEach((e) => {
+              e.Date = moment(e.Date).format('ddd, MMM DD YYYY');
+              e.longTitle = e.Title.length > 23 ? e.Title : false;
+              e.Title =
+               e.Title.length > 23 ? `${e.Title.slice(0, 23)}...` : e.Title;
+            });
+            res.json(data);
+          });
+      });
+  });
 };
